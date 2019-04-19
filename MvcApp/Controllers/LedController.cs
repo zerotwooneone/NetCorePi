e0365df753b0fef6b6e4fbcb97f21a0dd4133a44
David Squire <squiredd@gmail.com>
2019-04-18 17:28:25 -0700

    api now flashes led

---
+++ MvcApp/Controllers/LedController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LedController : ControllerBase
+    {
+        private static readonly Lazy<GpioController> LazyController = new Lazy<GpioController>(() =>
+        {
+            GpioController controller = new GpioController();
+            controller.OpenPin(Pin, PinMode.Output);
+            return controller;
+        });
+
+        const int Pin = 17;
+        const int LightTimeInMilliseconds = 1000;
+        const int DimTimeInMilliseconds = 200;
+        
+        // GET: api/Led
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Light for {LightTimeInMilliseconds}ms");
+                Controller.Write(Pin, PinValue.High);
+                await Task.Delay(TimeSpan.FromMilliseconds(LightTimeInMilliseconds));
+                Console.WriteLine($"Dim for {DimTimeInMilliseconds}ms");
+                Controller.Write(Pin, PinValue.Low);
+                await Task.Delay(TimeSpan.FromMilliseconds(DimTimeInMilliseconds));
+            }
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        private GpioController Controller => LazyController.Value;
+
+        // GET: api/Led/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Led
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Led/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
