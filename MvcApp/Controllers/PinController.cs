63f9caa933a1dbf60d23c7bef87aa52959321254
David Squire <squiredd@gmail.com>
2019-04-21 10:34:00 -0700

    failed attempt to use temp and humidity sensor

--- MvcApp/Controllers/PinController.cs
+++ MvcApp/Controllers/PinController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Device.Gpio;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,27 +14,83 @@ namespace MvcApp.Controllers
         private static readonly Lazy<GpioController> LazyController = new Lazy<GpioController>(() =>
         {
             GpioController controller = new GpioController();
-            controller.OpenPin(Pin, PinMode.Output);
+            controller.OpenPin(DhtPin, PinMode.Output);
             return controller;
         });
 
-        const int Pin = 17;
-        const int LightTimeInMilliseconds = 1000;
-        const int DimTimeInMilliseconds = 200;
-        
+        const int DhtPin = 4;
+        private static readonly TimeSpan TempHumLowDelay = TimeSpan.FromMilliseconds(18);
+        private static readonly TimeSpan TempHumHighDelay = TimeSpan.FromMilliseconds(40);
+        private static readonly TimeSpan TempHumReadDelay = TimeSpan.FromMilliseconds(1);
+        private const int MaxTimings = 85;
+
+
         // GET: api/Pin
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            for (int i = 0; i < 4; i++)
+            try
             {
-                Console.WriteLine($"Light for {LightTimeInMilliseconds}ms");
-                Controller.Write(Pin, PinValue.High);
-                await Task.Delay(TimeSpan.FromMilliseconds(LightTimeInMilliseconds));
-                Console.WriteLine($"Dim for {DimTimeInMilliseconds}ms");
-                Controller.Write(Pin, PinValue.Low);
-                await Task.Delay(TimeSpan.FromMilliseconds(DimTimeInMilliseconds));
+                Controller.SetPinMode(DhtPin, PinMode.Output);
+                Controller.Write(DhtPin, PinValue.Low);
+            
+                await Task.Delay(TempHumLowDelay);
+
+                Controller.Write(DhtPin, PinValue.High);
+
+                await Task.Delay(TempHumHighDelay);
+
+                Controller.SetPinMode(DhtPin, PinMode.Input);
+                var laststate = PinValue.High;
+                var dht11_dat = new byte[] {0, 0, 0, 0, 0};
+                int j = 0;
+                double f;
+
+                for (int i = 0; i < MaxTimings; i++ )
+                {
+                    int counter = 0;
+                    while ( Controller.Read(DhtPin) == laststate )
+                    {
+                        counter++;
+                        await Task.Delay(TempHumReadDelay);
+                        if ( counter == 255 )
+                        {
+                            break;
+                        }
+                    }
+                    laststate = Controller.Read(DhtPin);
+ 
+                    if ( counter == 255 )
+                        break;
+ 
+                    if ( (i >= 4) && (i % 2 == 0) )
+                    {
+                        dht11_dat[j / 8] <<= 1;
+                        if ( counter > 16 )
+                            dht11_dat[j / 8] |= 1;
+                        j++;
+                    }
+                }
+ 
+                f = dht11_dat[2] * 9.0 / 5.0 + 32;
+                Console.WriteLine( $"dht11_dat[0].dht11_dat[1] = {dht11_dat[0]}.{dht11_dat[1]} \n dht11_dat[2].dht11_dat[3] = {dht11_dat[2]}.{dht11_dat[3]} \n F ={f} \n");
+
+                if ( (j >= 40) &&
+                     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+                {
+                    
+                }else  {
+                    Console.WriteLine( "Data not good, skip\n" );
+                }
+                Controller.SetPinMode(DhtPin, PinMode.Output);
+                Controller.Write(DhtPin, PinValue.Low);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n{e}\n");
+                throw;
+            }
+
             return Ok(new string[] { "value1", "value2" });
         }
 
